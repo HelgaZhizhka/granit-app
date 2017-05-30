556ef9df4694cd3a09cdb7bcfab4506d167b629e
Olga Zhizhka <helgaj@yandex.ru>
2017-05-30 23:40:29 +0300

    about-mobile,  about-us-mobile, catalog-page1-mobile, catalog-page4-mobile, catalog-stone-mobile, products-stone-mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,20 @@
 'use strict';
 $(function () {
-
+	var $dropdownLink           = $('.js-dropdown-link'), //dropdown link for open/close filter mobile
+			$filterMobile						= $('.filter_mobile'); //in filter mobile
+	//dropdown filter color on mobile
+	$dropdownLink.on('click', function(e){
+		if (!($filterMobile).is(e.target)	&& $filterMobile.has(e.target).length === 0) {
+			$(this).toggleClass('active');
+		}
+	});
+	$('body').on('click touchstart', function(e) {
+		if (!($dropdownLink).is(e.target) && $dropdownLink.has(e.target).length === 0
+			&& !($filterMobile).is(e.target)
+			&& $filterMobile.has(e.target).length === 0
+			&& $filterMobile.is(':visible')) {
+			$dropdownLink.removeClass('active');
+		}
+	});
 	
 });
